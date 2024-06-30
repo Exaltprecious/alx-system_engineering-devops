28690cd092843fb33ea4982a339a7f9b04026ca9
Exaltprecious <peculiarchristj1234@gmail.com>
2024-06-30 12:24:18 -0700

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+using the puppet to make configurations to my file
+
+file { '/etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	",
+
+}

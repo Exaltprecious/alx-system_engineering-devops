08179867f69ffade45a4fba1974ac2a6e5a4cfe9
Exaltprecious <peculiarchristj1234@gmail.com>
2024-08-16 11:25:23 -0700

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-# Puppet manifest to fix a bug in wp-setings.php
+# Creat a manifest that fix all termintion of phpp.
 
-exec { 'fix the php extension issue':
-command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
 }
